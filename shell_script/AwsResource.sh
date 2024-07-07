114f41c73755dd112f5fb67363cc7daab63bdeb2
OmSantoshHarsule <162496189+OmSantoshHarsule@users.noreply.github.com>
2024-07-07 17:44:46 +0530

    Enter the file

--- shell_script/AwsResource.sh
+++ shell_script/AwsResource.sh
@@ -3,22 +3,22 @@
 #Author: Om Harsule
 #Date: 19th June
 #Version:1
-#This script give the information regarding the AWS resource like EC2 instance, IAM user, S3 Buckets, Lambda Function
+#This script give the information regarding the AWS Resource like EC2 instance, IAM user, S3 Buckets, Lambda Function
 ##########################
 set -x
 
-date >> Resource
+date >> ./shell_script/AWS.txt
 
-echo "List of EC2 instance" >> Resource
-aws ec2 describe-instance | jq '.Reservations[].Instances[].InstanceId' >> Resource
+echo "List of EC2 instance" >> ./shell_script/AWS.txt
+aws ec2 describe-instance | jq '.Reservations[].Instances[].InstanceId' >> ./shell_script/AWS.txt
 
-echo " List of IAM user " >> Resource
-aws iam list-users >> Resource
+echo " List of IAM user " >> ./shell_script/AWS.txt
+aws iam list-users >> ./shell_script/AWS.txt
 
-echo " List of S3 buckets " >> Resource
-aws s3 ls >> Resource
+echo " List of S3 buckets " >> ./shell_script/AWS.txt
+aws s3 ls >> ./shell_script/AWS.txt
 
-echo " List of Lambda Function " >> Resource
-aws lambda list-functions | jq '.Functions[].FunctionName' >> Resource
-aws lambda list-functions | jq '.Functions[].FunctionArn' >> Resource
+echo " List of Lambda Function " >> ./shell_script/AWS.txt
+aws lambda list-functions | jq '.Functions[].FunctionName' >> ./shell_script/AWS.txt
+aws lambda list-functions | jq '.Functions[].FunctionArn' >> ./shell_script/AWS.txt
 
